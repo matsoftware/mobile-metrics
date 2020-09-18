e12d603c03847bd3a1a415fd16267ab19e346e43
Mattia Campolese <info@matsoftware.net>
2020-09-18 16:38:24 +0100

    Add status check API

--- api/index.js
+++ api/index.js
@@ -14,6 +14,11 @@ app.use(bodyParser.json());
 // Parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Welcome route (unhaut)
+app.get('/', (req, res) => {
+  res.send(`<h1>Welcome to mobile-metrics-api. UP</h1>`);
+})
+
 // Authorization
 app.use(passport.initialize())
 app.use(auth)
@@ -22,10 +27,6 @@ app.use(auth)
 
 require("./app/routes/app_size.routes")(app);
 require("./app/routes/code_metrics.routes")(app);
-
-app.get('/', (req, res) => {
-  res.send(`<h1>Welcome to mobile-metrics interface ${req.body.user}</h1>`);
-})
 
 // START
 
